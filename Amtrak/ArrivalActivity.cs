81f499d2f4ca99b0ce9787c7551e70fba1ca4d6a
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-11-19 18:06:59 -0600

    Add project files.

---
+++ Amtrak/ArrivalActivity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Amtrak
+{
+    [Activity(Label = "ArrivalActivity")]
+    public class ArrivalActivity : Android.App.Activity
+    {
+        TextView arrival;
+        private ISharedPreferences prefs = Application.Context.GetSharedPreferences("APP_DATA", FileCreationMode.Private);
+
+        int h, m, l;
+        string hours, minutes, length;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_arrival);
+            // Create your application here
+            arrival = FindViewById<TextView>(Resource.Id.txtViewArrival);
+            hours = prefs.GetString("hours", "00");
+            minutes = prefs.GetString("minutes", "00");
+            length = prefs.GetString("length", "0");
+            addTime();
+        }
+
+        private void addTime()
+        {
+
+            try
+            {
+                h = int.Parse(hours);
+                m = int.Parse(minutes);
+                l = int.Parse(length);
+            }catch(Exception e)
+            {
+
+            }
+            
+            arrival.Text = Convert.ToString(25 % 23);
+            int tempH = 0, tempM = 0;
+            if (l % 60 == 0)
+            {
+                tempH = 1 / 60;
+                if (h + tempH > 23)
+                {
+                    h = (tempH + h) - 24;
+                    if (h <= 12)
+                    {
+                        arrival.Text = "0" + Convert.ToString(h);
+                    }
+                    else
+                    {
+                        arrival.Text = Convert.ToString(h);
+                    }
+                    
+                    
+                }
+                else
+                {
+                    h += tempH;
+                    arrival.Text = Convert.ToString(h);
+                }
+            }
+            
+        }
+        
+    }
+}
---
+++ Amtrak/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using System;
+
+namespace Amtrak
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnSubmit;
+        EditText inputHours, inputMinutes, inputLength;
+        private ISharedPreferences prefs = Application.Context.GetSharedPreferences("APP_DATA", FileCreationMode.Private);
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            Intent arrivalActivity = new Intent(this, typeof(ArrivalActivity));
+            btnSubmit = FindViewById<Button>(Resource.Id.btnSubmit);
+            inputHours = FindViewById<EditText>(Resource.Id.inputHours);
+            inputMinutes = FindViewById<EditText>(Resource.Id.inputMinutes);
+            inputLength = FindViewById<EditText>(Resource.Id.inputLength);                        
+
+            btnSubmit.Click += delegate
+            {
+                ISharedPreferencesEditor editor = prefs.Edit();
+                editor.PutString("hours", inputHours.Text);
+                editor.PutString("minutes", inputMinutes.Text);
+                editor.PutString("length", inputLength.Text);
+
+                //write key pairs to SP
+                editor.Apply();
+                StartActivity(arrivalActivity);
+            };
+
+            inputHours.TextChanged += delegate
+            {
+                try
+                {
+                    if (int.Parse(inputHours.Text.ToString()) > 23)
+                    {
+                        Toast.MakeText(this, "Hours must be equal to 23 or smaller", ToastLength.Short).Show();
+                    }
+                }
+                catch(Exception e)
+                {
+
+                }
+                
+            };
+
+            inputMinutes.TextChanged += delegate
+            {
+                try
+                {
+                    if (int.Parse(inputMinutes.Text.ToString()) > 59)
+                    {
+                        Toast.MakeText(this, "Minutes must be equal to 59 or smaller", ToastLength.Short).Show();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                
+            };
+
+            inputLength.TextChanged += delegate
+            {
+                try
+                {
+                    if (int.Parse(inputLength.Text.ToString()) > 1500)
+                    {
+                        Toast.MakeText(this, "Length must be equal to 1500 or smaller", ToastLength.Short).Show();
+                    }
+                }
+                catch (Exception e)
+                {
+                    
+                }
+
+            };
+        }
+    }
+}
